9bbd240722b512ccbe0b6dce00291d996c959d69
Guanghui Wang <wghglory89@gmail.com>
2016-02-22 19:12:58 -0800

    update controller

--- Guanghui.OA.Webapp/Controllers/BookController.cs
+++ Guanghui.OA.Webapp/Controllers/BookController.cs
@@ -19,24 +19,25 @@ namespace Guanghui.OA.Webapp.Controllers
         {
             #region call bll
             int totalCount = 0;
-            return View(BookService.LoadPageEntities(20, 1, out totalCount, u => true, true, o => o.Id));
+            var list = BookService.LoadPageEntities(20, 1, out totalCount, u => true, true, o => o.Id);
+            //return View(list);
             #endregion
 
             #region call api
-            //using (var client = new HttpClient())
-            //{
-            //    client.BaseAddress = new Uri("http://localhost:48647/");
-            //    client.DefaultRequestHeaders.Accept.Clear();
-            //    client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-            //    var response = client.GetAsync("api/books").Result;
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        string responseString = response.Content.ReadAsStringAsync().Result;
-            //        IEnumerable<Book> book = JsonConvert.DeserializeObject<IEnumerable<Book>>(responseString);  //iqueryable cannot work
-            //        return View(book);
-            //    }
-            //}
-            return null; 
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("http://localhost:48647/");
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var response = client.GetAsync("api/books").Result;
+                if (response.IsSuccessStatusCode)
+                {
+                    string responseString = response.Content.ReadAsStringAsync().Result;
+                    IEnumerable<Book> book = JsonConvert.DeserializeObject<IEnumerable<Book>>(responseString);  //iqueryable cannot work
+                    return View(book);
+                }
+            }
+            return null;
             #endregion
 
         }
