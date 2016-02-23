76b99bce9a1c333e6b8cecff1c469cfb11f1fe2f
Guanghui Wang <wghglory89@gmail.com>
2016-02-22 17:52:22 -0800

    spring.net and abstract factory architecture, T4

---
+++ Guanghui.OA.DALFactory/DalFactoryTT.cs
@@ -0,0 +1,150 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Guanghui.OA.IDAL;
+
+namespace Guanghui.OA.DALFactory
+{
+    /// <summary>
+    /// 简单工厂或者抽象工厂。
+    /// </summary>
+    public partial class DalFactory
+    {  
+		
+		public static IActionInfoDal GetActionInfoDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".ActionInfoDal", true);
+			//return obj as IActionInfoDal;
+
+			string fullClassName = NameSpace + ".ActionInfoDal";
+            return CreateInstance(fullClassName) as IActionInfoDal;
+        }
+		
+		public static IBookDal GetBookDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".BookDal", true);
+			//return obj as IBookDal;
+
+			string fullClassName = NameSpace + ".BookDal";
+            return CreateInstance(fullClassName) as IBookDal;
+        }
+		
+		public static IDepartmentDal GetDepartmentDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".DepartmentDal", true);
+			//return obj as IDepartmentDal;
+
+			string fullClassName = NameSpace + ".DepartmentDal";
+            return CreateInstance(fullClassName) as IDepartmentDal;
+        }
+		
+		public static IOrderDal GetOrderDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".OrderDal", true);
+			//return obj as IOrderDal;
+
+			string fullClassName = NameSpace + ".OrderDal";
+            return CreateInstance(fullClassName) as IOrderDal;
+        }
+		
+		public static IR_User_ActionInfoDal GetR_User_ActionInfoDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".R_User_ActionInfoDal", true);
+			//return obj as IR_User_ActionInfoDal;
+
+			string fullClassName = NameSpace + ".R_User_ActionInfoDal";
+            return CreateInstance(fullClassName) as IR_User_ActionInfoDal;
+        }
+		
+		public static IRoleDal GetRoleDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".RoleDal", true);
+			//return obj as IRoleDal;
+
+			string fullClassName = NameSpace + ".RoleDal";
+            return CreateInstance(fullClassName) as IRoleDal;
+        }
+		
+		public static ISearchLogDal GetSearchLogDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".SearchLogDal", true);
+			//return obj as ISearchLogDal;
+
+			string fullClassName = NameSpace + ".SearchLogDal";
+            return CreateInstance(fullClassName) as ISearchLogDal;
+        }
+		
+		public static ISearchLogGroupByDal GetSearchLogGroupByDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".SearchLogGroupByDal", true);
+			//return obj as ISearchLogGroupByDal;
+
+			string fullClassName = NameSpace + ".SearchLogGroupByDal";
+            return CreateInstance(fullClassName) as ISearchLogGroupByDal;
+        }
+		
+		public static IUserDal GetUserDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".UserDal", true);
+			//return obj as IUserDal;
+
+			string fullClassName = NameSpace + ".UserDal";
+            return CreateInstance(fullClassName) as IUserDal;
+        }
+		
+		public static IUserExtDal GetUserExtDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".UserExtDal", true);
+			//return obj as IUserExtDal;
+
+			string fullClassName = NameSpace + ".UserExtDal";
+            return CreateInstance(fullClassName) as IUserExtDal;
+        }
+		
+		public static IWF_InstanceDal GetWF_InstanceDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".WF_InstanceDal", true);
+			//return obj as IWF_InstanceDal;
+
+			string fullClassName = NameSpace + ".WF_InstanceDal";
+            return CreateInstance(fullClassName) as IWF_InstanceDal;
+        }
+		
+		public static IWF_StepDal GetWF_StepDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".WF_StepDal", true);
+			//return obj as IWF_StepDal;
+
+			string fullClassName = NameSpace + ".WF_StepDal";
+            return CreateInstance(fullClassName) as IWF_StepDal;
+        }
+		
+		public static IWF_TempDal GetWF_TempDal()
+        {
+            
+            //object obj = Assembly.Load(AssemblyName).CreateInstance(AssemblyName + ".WF_TempDal", true);
+			//return obj as IWF_TempDal;
+
+			string fullClassName = NameSpace + ".WF_TempDal";
+            return CreateInstance(fullClassName) as IWF_TempDal;
+        }
+	}
+
+}
---
+++ Guanghui.OA.DALFactory/DbSessionTT.cs
@@ -0,0 +1,186 @@
+﻿ 
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Guanghui.OA.EFDAL;
+using Guanghui.OA.IDAL;
+
+namespace Guanghui.OA.DALFactory
+{
+    public partial class DbSession :IDbSession
+    {
+   
+	
+		private IActionInfoDal _ActionInfoDal;
+		public IActionInfoDal ActionInfoDal
+		{
+			get
+			{
+				if (_ActionInfoDal == null)
+				{
+					_ActionInfoDal = DalFactory.GetActionInfoDal();
+				}
+				return _ActionInfoDal;
+			}
+		}
+	
+		private IBookDal _BookDal;
+		public IBookDal BookDal
+		{
+			get
+			{
+				if (_BookDal == null)
+				{
+					_BookDal = DalFactory.GetBookDal();
+				}
+				return _BookDal;
+			}
+		}
+	
+		private IDepartmentDal _DepartmentDal;
+		public IDepartmentDal DepartmentDal
+		{
+			get
+			{
+				if (_DepartmentDal == null)
+				{
+					_DepartmentDal = DalFactory.GetDepartmentDal();
+				}
+				return _DepartmentDal;
+			}
+		}
+	
+		private IOrderDal _OrderDal;
+		public IOrderDal OrderDal
+		{
+			get
+			{
+				if (_OrderDal == null)
+				{
+					_OrderDal = DalFactory.GetOrderDal();
+				}
+				return _OrderDal;
+			}
+		}
+	
+		private IR_User_ActionInfoDal _R_User_ActionInfoDal;
+		public IR_User_ActionInfoDal R_User_ActionInfoDal
+		{
+			get
+			{
+				if (_R_User_ActionInfoDal == null)
+				{
+					_R_User_ActionInfoDal = DalFactory.GetR_User_ActionInfoDal();
+				}
+				return _R_User_ActionInfoDal;
+			}
+		}
+	
+		private IRoleDal _RoleDal;
+		public IRoleDal RoleDal
+		{
+			get
+			{
+				if (_RoleDal == null)
+				{
+					_RoleDal = DalFactory.GetRoleDal();
+				}
+				return _RoleDal;
+			}
+		}
+	
+		private ISearchLogDal _SearchLogDal;
+		public ISearchLogDal SearchLogDal
+		{
+			get
+			{
+				if (_SearchLogDal == null)
+				{
+					_SearchLogDal = DalFactory.GetSearchLogDal();
+				}
+				return _SearchLogDal;
+			}
+		}
+	
+		private ISearchLogGroupByDal _SearchLogGroupByDal;
+		public ISearchLogGroupByDal SearchLogGroupByDal
+		{
+			get
+			{
+				if (_SearchLogGroupByDal == null)
+				{
+					_SearchLogGroupByDal = DalFactory.GetSearchLogGroupByDal();
+				}
+				return _SearchLogGroupByDal;
+			}
+		}
+	
+		private IUserDal _UserDal;
+		public IUserDal UserDal
+		{
+			get
+			{
+				if (_UserDal == null)
+				{
+					_UserDal = DalFactory.GetUserDal();
+				}
+				return _UserDal;
+			}
+		}
+	
+		private IUserExtDal _UserExtDal;
+		public IUserExtDal UserExtDal
+		{
+			get
+			{
+				if (_UserExtDal == null)
+				{
+					_UserExtDal = DalFactory.GetUserExtDal();
+				}
+				return _UserExtDal;
+			}
+		}
+	
+		private IWF_InstanceDal _WF_InstanceDal;
+		public IWF_InstanceDal WF_InstanceDal
+		{
+			get
+			{
+				if (_WF_InstanceDal == null)
+				{
+					_WF_InstanceDal = DalFactory.GetWF_InstanceDal();
+				}
+				return _WF_InstanceDal;
+			}
+		}
+	
+		private IWF_StepDal _WF_StepDal;
+		public IWF_StepDal WF_StepDal
+		{
+			get
+			{
+				if (_WF_StepDal == null)
+				{
+					_WF_StepDal = DalFactory.GetWF_StepDal();
+				}
+				return _WF_StepDal;
+			}
+		}
+	
+		private IWF_TempDal _WF_TempDal;
+		public IWF_TempDal WF_TempDal
+		{
+			get
+			{
+				if (_WF_TempDal == null)
+				{
+					_WF_TempDal = DalFactory.GetWF_TempDal();
+				}
+				return _WF_TempDal;
+			}
+		}
+	}
+
+}
